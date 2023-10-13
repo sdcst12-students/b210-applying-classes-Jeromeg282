0512ee883b28991bccb55c0b76597231db1e2ac8
Jeromeg282 <113056411+Jeromeg282@users.noreply.github.com>
2023-10-13 13:30:10 -0700

    .

--- assignment.py
+++ assignment.py
@@ -1,9 +1,33 @@
 #! python3
 
+
+import random
+import math
+
 class game:
 
 
-  def __init__(self):
+  def __init__(self, name):
+    if(name=='rock'):
+      num=0
+    elif(name=="spock"):
+      num=1
+    elif(name=="paper"):
+      num=2
+    elif(name=="lizard"):
+      num=3
+    elif(name=="scissors"):
+      num=4
+    else:
+      num=None
+      return num
+
+
+
+
+
+
+
     pass
 
 

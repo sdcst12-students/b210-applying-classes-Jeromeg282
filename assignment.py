4edba7dc52c17755c3e6617a63d476c20390f171
Keita354 <113056447+Keita354@users.noreply.github.com>
2023-12-07 18:19:57 -0800

    .

--- assignment.py
+++ assignment.py
@@ -2,47 +2,56 @@
 
 
 import random
-import math
-
-class game:
 
 
-  def __init__(self, name):
-    self.valid_moves = ["rock", "spock", "paper", "lizard", "scissors"]
-    if name in self.valid_moves:
-      self.player_choice = name
-      self.computer_choice = random.choice(self.valid_moves)
-    else:
-      print("Invalid input. Please choose from: rock, spock, paper, lizard, or scissors.")
-  def winnercheck(self):
-    if self.player_choice == self.computer_choice:
-      return "It's a tie"
-    if(self.player_choice == "rock" and self.computer_choice in ["lizard", "scissors"]) or \
-      (self.player_choice == "spock" and self.computer_choice in ["rock", "scissors"]) or \
-      (self.player_choice == "paper" and self.computer_choice in ["spock", "rock"]) or \
-      (self.player_choice == "lizard" and self.computer_choice in ["spock", "paper"]) or \
-      (self.player_choice == "scissors" and self.computer_choice in ["lizard", "paper"]):
-      return "You win"
-    else:
-      return "You lost"
+class Game:
 
-  def play_game(self):
-        print(f"Player's choice: {self.player_choice}")
-        print(f"Computer's choice: {self.computer_choice}")
-        print(self.winnercheck())
+    def __init__(self):
+        self.choices = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+        self.p_score = 0
+        self.c_score = 0
 
+    def play(self):
+        while True:
+            p_choice = self.get_p_choice()
+            if p_choice == 'quit':
+                print("Thanks for playing! Final scores:")
+                print(f"Player: {self.p_score}")
+                print(f"Computer: {self.c_score}")
+                break
+            c_choice = self.get_c_choice()
+            self.display_choices(p_choice, c_choice)
+            self.determine_winner(p_choice, c_choice)
 
-while True:
-    i=input("Enter your move: 'rock, paper, scissors, lizard, spock. or 'quit' to exit out of the game: ").lower()
+    def get_p_choice(self):
+        while True:
+            choice = input("Enter your choice (rock/paper/scissors/lizard/spock) or 'quit' to end the game: ").lower()
+            if choice in self.choices or choice == 'quit':
+                return choice
+            else:
+                print("Invalid input. Please choose from 'rock', 'paper', 'scissors', 'lizard', 'spock', or 'quit'.")
 
-    if i =='quit':
-      break
+    def get_c_choice(self):
+        return random.choice(self.choices)
 
-    game=game(i)
-    game.play_game()
+    def display_choices(self, p_choice, c_choice):
+        print(f"Player chooses {p_choice}")
+        print(f"Computer chooses {c_choice}")
 
-pass
+    def determine_winner(self, p_choice, c_choice):
+        if p_choice == c_choice:
+            print("It's a tie!")
+        elif (p_choice == 'rock' and (c_choice == 'scissors' or c_choice == 'lizard')) or \
+             (p_choice == 'paper' and (c_choice == 'rock' or c_choice == 'spock')) or \
+             (p_choice == 'scissors' and (c_choice == 'paper' or c_choice == 'lizard')) or \
+             (p_choice == 'lizard' and (c_choice == 'spock' or c_choice == 'paper')) or \
+             (p_choice == 'spock' and (c_choice == 'scissors' or c_choice == 'rock')):
+            print("Player wins!")
+            self.p_score += 1
+        else:
+            print("Computer wins!")
+            self.c_score += 1
 
-
-# This is the only command allowed that is not in the class template. All code must be done there.
-g = game()
+if __name__ == "__main__":
+    g = Game()
+    g.play()
